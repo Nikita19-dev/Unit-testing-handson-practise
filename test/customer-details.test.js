8eca2c4c5e5ac39dd12e898b9e7f222c15c51c91
Nikita andoor <92866004+Nikita19-dev@users.noreply.github.com>
2024-10-10 15:34:56 +0530

    add more test cases to customer -details

--- test/customer-details.test.js
+++ test/customer-details.test.js
@@ -1,6 +1,8 @@
 import { html, fixture, expect } from '@open-wc/testing';
 import { stub } from 'sinon';
 import '../src/Customer/Customer-details.js';
+import sinon from 'sinon'; 
+import { Router } from '@vaadin/router';
 
 describe('customer details', () => {
   // Write test cases inside this block
@@ -11,5 +13,31 @@ describe('customer details', () => {
   });
 
 
+  afterEach(() => {
+    sinon.restore();
+  });
+
+  it('should handle validation errors correctly', async () => {
+    const firstNameInput = element.shadowRoot.querySelector('#first_name');
+    firstNameInput.value = 'AB'; // Invalid input
+    firstNameInput.dispatchEvent(new Event('input'));
   
+    const submitButton = element.shadowRoot.querySelector('#nextbtn');
+    submitButton.click(); 
+    await element.updateComplete;
+    expect(firstNameInput.classList.contains('error-handle')).to.be.true;
 });
+
+
+it('navigates to EMI details page when  clicked ', async () => {
+  const el = await fixture(html`<customer-details></customer-details>`);
+  const continueButton = el.shadowRoot.querySelector('.backbg-btn-color');
+
+  const navigateSpy = sinon.spy(Router, 'go');
+
+  continueButton.click(); 
+  expect(navigateSpy).to.have.been.calledWith('/emidetails');
+  navigateSpy.restore(); 
+});
+
+})
